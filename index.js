b985aed94aa201d33e3040948c19806e43771444
Lucian Barbu <112772067+Luke-Brb@users.noreply.github.com>
2024-06-18 13:33:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -238,3 +238,7 @@ function shoot(e) {
 function detectCollisionBullet(a, b) {
 	return a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
 }
+
+function detectCollisionEnemies(a, b) {
+	return a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
+}
